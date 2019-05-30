799d36037c244ff645b487ceaf926c72436d1284
adetbekov <dosya@inbox.ru>
2019-05-31 00:01:59 +0600

    I18n instalation

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,39 +1,92 @@
-module.exports = {
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: 'promo-portal',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-  /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
-  /*
-  ** Build configuration
-  */
-  build: {
-    /*
-    ** Run ESLint on save
-    */
-    extend (config, { isDev, isClient }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
-  }
-}
+const pkg = require('./package')
+const nodeExternals = require("webpack-node-externals")
 
+module.exports = {
+    /*
+     ** Headers of the page
+     */
+    head: {
+        title: 'promo-portal',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    },
+    /*
+     ** Customize the progress bar color
+     */
+    loading: { color: '#3B8070' },
+    plugins: [
+        "~/plugins/axios",
+        // "~/plugins/font-awesome",
+        { src: "~/plugins/nuxt-client-init.js", ssr: false },
+        // '@/plugins/element-ui'
+    ],
+    modules: [
+        '@nuxtjs/axios',
+        'nuxt-sass-resources-loader', ["nuxt-i18n", require("./i18n/config")],
+        "nuxt-mq"
+    ],
+    mq: {
+        defaultBreakpoint: 'default',
+        breakpoints: {
+            mobile: 450,
+            tablet: 900,
+            laptop: 1250,
+            desktop: Infinity
+        }
+    },
+    axios: {
+        host: "https://api.rglservice.kz",
+        prefix: "/v2/180713142003"
+    },
+
+    /*
+     ** Build configuration
+     */
+    build: {
+        vendor: ["vue-awesome"],
+        // postcss: [
+        //     require("postcss-nested")(),
+        //     require("postcss-responsive-type")(),
+        //     require("postcss-hexrgba")(),
+        //     require("autoprefixer")()
+        // ],
+        extend(config, { isDev, isClient, isServer }) {
+            if (isDev && isClient) {
+                config.module.rules.push({
+                        enforce: "pre",
+                        test: /\.(vue)$/,
+                        loader: "vue-loader",
+                        exclude: /(node_modules)/,
+                        options: {
+                            fix: true
+                        }
+                    }, {
+                        resourceQuery: /blockType=i18n/,
+                        type: 'javascript/auto',
+                        loader: '@kazupon/vue-i18n-loader'
+                    })
+                    // config.module.rules.find(
+                    //         el => el.loader === "vue-loader"
+                    //     ).options.loaders.i18n =
+                    //     "@kazupon/vue-i18n-loader"
+            }
+
+            if (isServer) {
+                config.externals = [
+                    nodeExternals({
+                        whitelist: [
+                            /es6-promise|\.(?!(?:js|json)$).{1,5}$/i,
+                            /^vue-awesome/
+                        ]
+                    })
+                ]
+            }
+        }
+    }
+}
